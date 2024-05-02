344ba1647b2b0b731043fe0da8171dcb363b480c
kimiasa <kimiasaedi0@gmail.com>
2024-05-01 22:45:00 -0500

    update

--- SSL.py
+++ SSL.py
@@ -1,45 +1,50 @@
-from math import sqrt
-
-from math import sqrt
+import math
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import init
 
-import time
-
-from .SSLFunction import SketchStructuredLinearFunction
 from .SSLFunction import ssl_linear
 from .Mapper_v2 import *
 
+BLOCK_K_SIZE_MIN = 64
 
 class SSL(nn.Module):
-    def __init__(self, in_features, out_features, bias=None, redn_factor=1, seed=1024):
+    def __init__(self, in_features: int, out_features: int, bias: bool = True, 
+                 redn_factor: int = 2, seed: int = 1024,
+                 device=None, dtype=None) -> None:
+        factory_kwargs = {'device': device, 'dtype': dtype}
         super(SSL, self).__init__()
-        
-        self.in_features = in_features
-        self.out_features = out_features
-        self.redn_factor = redn_factor
-        self.seed = seed
 
         assert redn_factor > 0 and (redn_factor & (redn_factor - 1)) == 0 # reduction factor should be power of 2
         self.redn_factor = redn_factor
         
-        self.weight = nn.Parameter(torch.zeros((out_features, int(in_features // redn_factor)), dtype=torch.float), requires_grad=True)
-        
-        self.init_scale = 1/sqrt(in_features)
-        nn.init.uniform_(self.weight.data, a=-
-                            self.init_scale, b=self.init_scale)
+        self.out_features = out_features         
+        self.red_in_features = (in_features  // redn_factor + BLOCK_K_SIZE_MIN - 1) // BLOCK_K_SIZE_MIN * BLOCK_K_SIZE_MIN        
 
-        #self.bias = None
-        #if bias is None:
-        #    self.bias = nn.Parameter(torch.zeros(
-        #        self.out_features, dtype=torch.float), requires_grad=True)
-            
-            
+        self.seed = seed
+
+        self.weight = nn.Parameter(torch.zeros((out_features, self.red_in_features), **factory_kwargs))
+
+        if bias:
+            self.bias = nn.Parameter(torch.empty(out_features, **factory_kwargs))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
         self.hasher = HasherFactory.get("uhash", self.seed)
         self.hasher = HasherFactory.get("uhash", self.seed)
+
+    def reset_parameters(self) -> None:
+        # Setting a=sqrt(5) in kaiming_uniform is the same as initializing with
+        # uniform(-1/sqrt(in_features), 1/sqrt(in_features)). 
+        init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if self.bias is not None:
+            fan_in, _ = init._calculate_fan_in_and_fan_out(self.weight)
+            bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
+            init.uniform_(self.bias, -bound, bound)
+            
 
     def forward(self, x):
         original_shape = x.shape
@@ -64,7 +69,6 @@ class SSL(nn.Module):
         dim_gt_2 = len(shape) > 2
         if (dim_gt_2):
             x = x.view(*shape[:-1], x.shape[-1]).contiguous()
-            x = x.view(*shape[:-1], x.shape[-1]).contiguous()
         return x
     
     
@@ -75,9 +79,5 @@ class SSL(nn.Module):
     def __repr__(self):        
         return "SketchStructuredLinear(in={}, out={}, compression={}, seed={}, saving={})".format(self.in_features, self.out_features, self.redn_factor, self.seed, self.saving)
     
-        return (self.weight.numel() / (self.in_features * self.out_features))
-
-    def __repr__(self):        
-        return "SketchStructuredLinear(in={}, out={}, compression={}, seed={}, saving={})".format(self.in_features, self.out_features, self.redn_factor, self.seed, self.saving)
     
     
